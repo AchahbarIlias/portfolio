5d004537065862eb16c26791e9ce98cb28fe904e
Ilias Achahbar <ilias.achahbar@student.ucll.be>
2021-10-12 15:07:42 +0200

    updated fonts and style

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -7,21 +7,37 @@
     </section>
 
     <section class="mx-4 text-center mt-40 flex-col justify-center">
-      <h1 class="mt-2 text-5xl sm:text-7xl text-textgreyish">Currulicum Vitea</h1>
-      <p class="mt-2 text-lg text-textpurple">Life and Work</p>
-      <div class="grid gap-4 justify-center mt-20 mx-10">
+      <h1 class="mt-2 text-5xl sm:text-7xl text-textgreyish">Life and Work</h1>
+      <p class="mt-2 text-lg text-textpurple">An insight into my life</p>
 
-        <div class="h-auto w-auto bg-white p-4">
+
+      <div class="bg-backgroundpurple mt-10 text-textgreyish font-extrabold">
+        <h2>#- - - Biography - - -#</h2>
+      </div>
+
+      <div class="bg-backgroundpurple mt-4 text-textgreyish p-6 mx-6 md:text-lg font-semibold">
+        <p>Student in Leuven who lives in Mechelen, Belgium. I am in my graduation year at the UCLL studying Applied Information Technology.</p>
+        <p class="mt-2">Currently fascinated by web development and all things around it.</p>
+      </div>
+
+
+      <div class="bg-backgroundpurple mt-10 text-textgreyish font-extrabold">
+        <h2>#- - - Work Experience - - -#</h2>
+      </div>
+
+      <div class="grid gap-4 justify-center mt-4 mx-6">
+
+        <div class="h-auto w-auto bg-white p-4 border-4 border-borderpurple font-semibold">
           <img class="mx-auto" src="../assets/emmaus.png" alt="">
           <h2 class="px-4">Intern • IT Department • Emmaüs vzw</h2>
         </div>
 
-        <div class="h-auto w-auto bg-white p-4">
+        <div class="h-auto w-auto bg-white p-4 border-4 border-borderpurple font-semibold">
           <img class="mx-auto" src="../assets/emmaus.png" alt="">
           <h2 class="px-4">Administrative Work • IT Department • Emmaüs vzw</h2>
         </div>
 
-        <div class="h-auto w-auto bg-white p-4">
+        <div class="h-auto w-auto bg-white p-4 border-4 border-borderpurple font-semibold">
           <img class="mx-auto" src="../assets/maarten.png" alt="">
           <h2 class="px-4">Cleaner • Cleaning Department • Sint-Maarten Mechelen</h2>
         </div>
@@ -29,76 +45,114 @@
       </div>
     </section>
 
-    <section class="mx-4 text-center mt-40 flex-col justify-center">
-      <h1 class="mt-2 text-5xl sm:text-7xl text-textgreyish">Career</h1>
-      <p class="mt-2 text-lg text-textpurple">Timeline of studying</p>
-      
-      <div class="mx-auto justify-center mt-20 flex flex-col lg:flex-row ">
 
-      <div class="mx-auto text-center lg:mx-0 lg:mt-6 h-8 w-40 bg-backgroundpurple rounded-xl">
-        <p class="text-2xl text-white">2016</p>
-      </div>
-
-      <div class="lg:mx-9 mx-auto h-20 w-2 bg-backgroundpurple transform lg:rotate-90"></div>
-
-      <div class="sm:w-80 lg:mx-0 mx-auto bg-backgroundpurple w-auto h-auto p-4 rounded-2xl	">
-        <p class="mt-2 text-2xl text-white">Network and IT</p>
-        <p class="mt-2 text-lg text-white">COLOMAplus</p>
-      </div>
-
-      <div class="lg:mx-9 mx-auto h-20 w-2 bg-backgroundpurple transform lg:rotate-90"></div>
-
-      <div class="mx-auto text-center lg:mx-0 lg:mt-6 h-8 w-40 bg-backgroundpurple rounded-xl">
-        <p class="text-2xl text-white">2018</p>
-      </div>
-
-      <div class="lg:mx-9 mx-auto h-20 w-2 bg-backgroundpurple transform lg:rotate-90"></div>
-
-      <div class="sm:w-80 lg:mx-0 mx-auto bg-backgroundpurple w-auto h-auto p-4 rounded-2xl	">
-        <p class="mt-2 text-2xl text-white">Applied Computer Science</p>
-        <p class="mt-2 text-md text-white">(Software Development)</p>
-        <p class="mt-2 text-lg text-white">UC Leuven-Limburg</p>
-      </div>
-
-      <div class="lg:mx-9 mx-auto h-20 w-2 bg-backgroundpurple transform lg:rotate-90"></div>
-
-      <div class="mx-auto text-center lg:mx-0 lg:mt-6 h-8 w-40 bg-backgroundpurple rounded-xl">
-        <p class="text-2xl text-white">Now</p>
-      </div>
-    </div>
-    </section>
-
-    <section class="mx-4 text-center mt-40 flex-col justify-center">
+      <section class="mx-4 text-center mt-40 flex-col justify-center">
       <h1 class="mt-2 text-5xl sm:text-7xl text-textgreyish">Skills</h1>
       <p class="mt-2 text-lg text-textpurple">...</p>
-      <div class="grid gap-4 grid-cols-2 justify-center mt-20 ">
+    <div class="grid sm:grid-cols-2 gap-4">
 
-        <div class="h-auto w-auto border-8 border-jsyellow p-4">
+
+<div>
+      <div class="bg-backgroundpurple mt-10 text-textgreyish font-extrabold">
+        <h2>#- - - Programming Languages - - -#</h2>
+      </div>
+
+      <div class="grid gap-4 md:grid-cols-2 justify-center mt-10 mx-8 font-semibold">
+
+        <div class="h-auto w-auto border-b-8 border-jsyellow p-4">
           <img class="h-20 mx-auto" src="../assets/javascript-logo.png" alt="">
           <h2 class="text-white">JavaScript</h2>
+          <div class="text-textpurple">
+            <ul>
+              <li>NodeJs</li>
+              <li>VueJS</li>
+              <li>Express</li>
+            </ul>
+          </div>
+
         </div>
 
-        <div class="h-auto w-auto border-8 border-javared p-4">
+        <div class="h-auto w-auto border-b-8 border-javared p-4">
           <img class="h-20 mx-auto" src="../assets/java-logo.png" alt="">
           <h2 class="text-white">Java</h2>
+          <div class="text-textpurple">
+            <ul>
+              <li>Spring</li>
+            </ul>
+          </div>
         </div>
 
-        <div class="h-auto w-auto border-8 border-csharppurple p-4">
+        <div class="h-auto w-auto border-b-8 border-csharppurple p-4">
           <img class="h-20 mx-auto" src="../assets/csharp.svg" alt="">
           <h2 class="text-white">C#</h2>
         </div>
 
-        <div class="h-auto w-auto border-8 border-phppurple p-4">
+        <div class="h-auto w-auto border-b-8 border-phppurple p-4">
           <img class="h-20 mx-auto" src="../assets/php-logo.png" alt="">
           <h2 class="text-white">PHP</h2>
         </div>
 
-        <div class="h-auto w-auto border-8 border-cplusplusblue p-4">
+        <div class="h-auto w-auto border-b-8 border-cplusplusblue p-4">
           <img class="h-20 mx-auto" src="../assets/cplusplus-logo.png" alt="">
           <h2 class="text-white">C++</h2>
         </div>
+
+        <div class="h-auto w-auto border-b-8 border-elixirpurple p-4">
+          <img class="h-20 mx-auto" src="../assets/elixir-logo.png" alt="">
+          <h2 class="text-white">Elixir</h2>
+          <div class="text-textpurple">
+            <ul>
+              <li>Phoenix</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="h-auto w-auto border-b-8 border-cssblue p-4">
+          <img class="h-20 mx-auto" src="../assets/css-logo.png" alt="">
+          <h2 class="text-white">CSS</h2>
+          <div class="text-textpurple">
+            <ul>
+              <li>Tailwindcss</li>
+              <li>Vuetify</li>
+            </ul>
+          </div>
+        </div>
       </div>
+
+</div>
+
+<div>
+      <div class="bg-backgroundpurple mt-10 text-textgreyish font-extrabold">
+        <h2>#- - - Languages - - -#</h2>
+      </div>
+
+            <div class="grid gap-4 grid-cols-1 justify-center mt-10 mx-8 font-semibold">
+
+        <div class="h-auto w-auto border-b-8 border-white p-4">
+          <h2 class="text-white">Dutch</h2>
+        </div>
+
+        <div class="h-auto w-auto border-b-8 border-white p-4">
+          <h2 class="text-white">English</h2>
+        </div>
+
+        <div class="h-auto w-auto border-b-8 border-white p-4">
+          <h2 class="text-white">French</h2>
+        </div>
+
+
+
+
+      </div>
+
+</div>
+
+
+
+    
+    </div>
     </section>
+    
 
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,23 @@ module.exports = {
     backgroundColor: theme => ({
      ...theme('colors'),
      'backgrounddark': '#0e1212',
-     'backgroundpurple': '#682ae9',
+     'backgroundpurple': '#A78BFA',
     }),
     textColor: theme => ({
       ...theme('colors'),
-      'textpurple': '#682ae9',
+      'textpurple': '#A78BFA',
       'textgreyish': '#dbdbdb',
     }),
     borderColor: theme => ({
       ...theme('colors'),
-      'borderpurple': '#682ae9',
+      'borderpurple': '#A78BFA',
       'jsyellow': '#F7E018',
       'javared': '#D12C52',
       'csharppurple': '#9A4993',
       'phppurple': "#787CB4",
-      'cplusplusblue': '#659BD3'
+      'cplusplusblue': '#659BD3',
+      'elixirpurple': '#340A49',
+      'cssblue': '#264DE4'
      })
   },
   variants: {
